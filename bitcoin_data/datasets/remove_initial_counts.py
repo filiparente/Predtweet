82046f00b81bed9f74de43fd80af2a6bd92eebcb
Filipa <filipaoliveirarente@gmail.com>
2020-04-02 19:02:36 +0100

    remove initial counts II

--- bitcoin_data/datasets/remove_initial_counts.py
+++ bitcoin_data/datasets/remove_initial_counts.py
@@ -8,46 +8,60 @@ def main():
 
     #Parser
     parser = argparse.ArgumentParser(description='Remove initial counts from the datasets because they are really small compared to the rest.')
-
     parser.add_argument('--dataset_path', default=".", help="Full path to the folder containing subfolders where the datasets are placed, for each combination of discretization unit and sliding window.")
-
     args = parser.parse_args()
     print(args)
 
     #select current directory
     directory = args.dataset_path
 
+    r = []                                                                                                            
+    subdirs = [x[0] for x in os.walk(directory)]                                                                            
+    for subdir in subdirs:                                                                                            
+        files = next(os.walk(subdir))[2]                                                                             
+        if (len(files) > 0):                                                                                          
+            for file in files:                                                                                        
+                r.append(subdir + "/" + file)    
+    r2 = [el for el in r if el.endswith('new_dataset.txt')]                                                                     
+
     #Get list of all folders in current datasets directory
-    for folder in os.walk(directory):
+    for filename in r2:
+        #for file in files:
+        #    #print os.path.join(subdir, file)
+        #    filepath = subdir + os.sep + file
+
+        #    if filepath.endswith(".html"):
+        #        print (filepath)
+
         #Get folder name
-        folder_name = folder[0]
+        #folder_name = folder[0]
 
-        if not folder_name in ['.', '..']:
-            start_idx = 0
+        #if not folder_name in ['.', '..']:
+        start_idx = 0
+        
+        #Load txt file containing the dataset
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+            for i in range(len(data['embeddings'])):
+                #Cut-off index
+                if data['embeddings'][i]['y']>1000:
+                    start_idx = i
+                    break
             
-            #Load txt file containing the dataset
-            with open(folder_name+'\\new_dataset.txt', "r") as f:
-                data = json.load(f)
+            #Cut dataset
+            list_of_dicts = data['embeddings']
+            data['embeddings'] = [list_of_dicts[i] for i in range(len(list_of_dicts)) if i>=start_idx]
+            
+            #Update start_date
+            data['start_date'] = (pd.to_datetime(data['start_date'])+timedelta(hours=start_idx)).value
 
-                for i in range(len(data['embeddings'])):
-                    #Cut-off index
-                    if data['embeddings'][i]['y']>1000:
-                        start_idx = i
-                        break
-                
-                #Cut dataset
-                list_of_dicts = data['embeddings']
-                data['embeddings'] = [list_of_dicts[i] for i in range(len(list_of_dicts)) if i>=start_idx]
-                
-                #Update start_date
-                data['start_date'] = (pd.to_datetime(data['start_date'])+timedelta(hours=start_idx)).value
-
-                #Store new dataset
-                with open(folder_name+'\\new_cut_dataset.txt', "w") as f2:
-                    json.dump(data, f2)
+            #Store new dataset
+            with open(filename[:-15]+'/new_cut_dataset.txt', "w") as f2:
+                json.dump(data, f2)
 
 
-                print("Done!")
+            print("Done!")
 
 if __name__== "__main__":
     main()
